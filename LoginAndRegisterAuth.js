048757865915242437828ea0296fbe11e9cb2772
JakubSabuda <jakubpiotrsabuda@gmail.com>
2021-04-21 13:38:26 +0200

    added login with google

--- LoginAndRegisterAuth.js
+++ LoginAndRegisterAuth.js
@@ -127,13 +127,41 @@ function signOut() {
       window.location.replace("../index.html");
       //alert("wylogowanie powiodło się"); //
       //Ponizej jest wylogowanie z fb
-      firebase.auth().signOut();
     })
     .catch((error) => {
       // Error podczas wylogowania
       alert("wylogowanie nie powiodło się");
     });
     
+}
+
+//Login with google
+function onSignIn(googleUser) {
+  console.log('Google Auth Response', googleUser);
+  // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+  var unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
+    unsubscribe();
+    // Check if we are already signed-in Firebase with the correct user.
+    if (!isUserEqual(googleUser, firebaseUser)) {
+      // Build Firebase credential with the Google ID token.
+      var credential = firebase.auth.GoogleAuthProvider.credential(
+          googleUser.getAuthResponse().id_token);
+
+      // Sign in with credential from the Google user.
+      firebase.auth().signInWithCredential(credential).catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    } else {
+      console.log('User already signed-in Firebase.');
+    }
+  });
 }
 
 //For services workers 
