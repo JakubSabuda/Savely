9e1e71cdd9711ed5cb8629cde94febc1fb965c46
JakubSabuda <jakubpiotrsabuda@gmail.com>
2021-04-21 14:07:54 +0200

    changing sesions sotrage for googleuer only

--- LoginAndRegisterAuth.js
+++ LoginAndRegisterAuth.js
@@ -139,7 +139,7 @@ function signOut() {
 function onSignIn(googleUser) {
   window.location.replace("/main/main.html");
   console.log('Google Auth Response', googleUser);
-  sessionStorage.setItem("username", googleUser.uid);
+  sessionStorage.setItem("username", googleUser);
   // We need to register an Observer on Firebase Auth to make sure auth is initialized.
   var unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
     unsubscribe();
